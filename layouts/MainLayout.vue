f4ec02f7dea2cb3f62983384bb36ed93e5f01a07
Bruno Dias <brunodg.dev@gmail.com>
2023-06-15 01:35:27 -0300

    feat: Top navbar created. On to SideBar

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,9 @@
+<template>
+    <TopNav/>
+    <div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  pages: true,
+  modules: ["nuxt-icon", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+});
